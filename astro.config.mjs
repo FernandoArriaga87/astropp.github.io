1bbe6c9a1298d3b817c6b0542a4941a15e31b541
Fernando <cbm0100687arriaga@gmail.com>
2025-01-28 15:35:45 -0600

    Configuración corregida para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://fernandoarriaga87.github.io/astropp.github.io',
+    base: '/astropp.github.io', 
     integrations: [mdx(), sitemap(), tailwind()],
 });
